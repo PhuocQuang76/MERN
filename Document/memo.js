ee679ab74861ecddb985ecb5dc600106ad0487f8
Aileen Quang <aileen@aileens-mbp.lan>
2024-04-16 00:53:57 -0700

    added files

---
+++ Document/memo.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+//1 *** REDUCE ****
+/*
+In JavaScript, the reduce() method is used to reduce an array to a single value. It executes a provided function for each element of the array, resulting in a single output value.
+
+The reduce() method takes two main parameters:
+
+A callback function that is executed on each element of the array. This callback function can take up to four arguments: accumulator, 
+current value, current index, and the array itself. An initial value for the accumulator (optional). If provided, 
+this value is used as the initial value for the accumulator in the first call to the callback function. If not provided, the first element of the 
+array is used as the initial accumulator value.The basic syntax for reduce() is as follows:
+*/
+
+array.reduce(callbackFunction, initialValue)
+
+//Here's a simple example of using reduce() to sum up all the elements in an array:
+
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, 0);
+
+console.log(sum); // Output: 15
+// In this example, the reduce() method is used to sum up all the elements in the numbers array starting 
+// with an initial value of 0 for the accumulator.
